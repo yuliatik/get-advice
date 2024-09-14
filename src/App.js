8bee572e6db3ea2ba6e55f7a7b6c0d8cf8a9c752
yuliatik <yulia.tik45@gmail.coim>
2024-09-14 22:59:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const[showAdvice, setShowAdvice] = useState([]);
+
+    useEffect(() =>{
+        const getAdvice = async() =>{
+        const response = await fetch('https://bored.api.lewagon.com/api/activity');
+        const data = await response.json();
+        console.log(data.activity);
+        setShowAdvice(data.activity);
+        }
+        getAdvice()
+    }, [])
+
+  
+   
+    return(
+        <div className='App'>
+
+            <div>
+                <p>{showAdvice}</p>
+            </div>
+
+            <div>        
+            <button onClick={setShowAdvice}>Get an advice</button>
+            </div>
+
+                   
+
+        </div>
+    )
+ }
+  export default App
+
+
+ 
+
+
